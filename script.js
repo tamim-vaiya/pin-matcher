3ccd870b0cbd3e84e30c82338faf634f001233d6
tamimvaiya <tamimvaiya.123@gmail.com>
2020-07-27 18:38:21 +0600

    mostly done, have to fix action left part

---
+++ script.js
@@ -0,0 +1,40 @@
+//getting random pin
+function getRandomNum(){
+  document.getElementById('randomPin').value = Math.floor(Math.random() * (10000 - 1000)) + 1000;
+  clearPinChecker()
+}
+//removing last number from pin checker
+function removeLastNum(){
+  pinChecker.value = pinChecker.value.slice(0,-1);
+}
+// clearing pin checker
+function clearPinChecker(){
+  pinChecker.value = "";
+}
+// working with submit button
+function submitBtn(){
+  const pinNum =  document.getElementById('randomPin').value;
+  const pinNumCheck = document.getElementById('pinChecker').value;
+  // const actionLeft = document.getElementById('action-left').innerText;
+  if (pinNumCheck.length == 4){
+    if (pinNum == pinNumCheck){
+      document.getElementById('match').style.display = 'block';
+      document.getElementById('not-match').style.display = 'none';
+    } 
+    else if (pinNum != pinNumCheck){
+      document.getElementById('match').style.display = 'none';
+      document.getElementById('not-match').style.display = 'block';
+      actionLeft();
+    }
+  } 
+  else {
+    const text = document.getElementById('pinChecker').value = 'Enter 4 digit number';
+    document.getElementById('match').style.display = 'none';
+    document.getElementById('not-match').style.display = 'block';
+    actionLeft();
+  }
+}
+// action left part
+// function actionLeft(){
+    // working with this part, coming soon
+// }
